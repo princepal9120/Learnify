6b63968c9a5e71816f5f38e020150917b8213018
Prince Pal <princepal9120@gmail.com>
2024-11-28 23:49:21 +0530

    course model initialize

--- client/src/features/api/authApi.js
+++ client/src/features/api/authApi.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery }  from "@reduxjs/toolkit/query/react"
-import { userLoggedIn } from "../authSlice";
+import { userLoggedIn, userLoggedOut } from "../authSlice";
 
 const USER_API= "http://localhost:8080/api/v1/user"
 
@@ -36,14 +36,30 @@ export const authApi = createApi({
             query: ()=>({
                 url: "logout",
                 method: "GET",
-            })
+            }),
+            async onQueryStarted(_, {queryFulfilled, dispatch}){
+                try {
+                    
+                    dispatch(userLoggedOut())
+                } catch (error) {
+                    console.log(error)
+                }
+            }
         }),
         loadUser: builder.query({
             query: ()=>({
                 url: "Profile",
                 method: "GET",
 
-            })
+            }),
+            async onQueryStarted(_, {queryFulfilled, dispatch}){
+                try {
+                    const result= await queryFulfilled;
+                    dispatch(userLoggedIn({user: result?.data.user}))
+                } catch (error) {
+                    console.log(error)
+                }
+            }
         }),
         updateUser: builder.mutation({
             query: (formData) =>({
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,32 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { appStore } from './app/store.js'
 import { Toaster } from './components/ui/sonner'
+import { useLoadUserQuery } from './features/api/authApi'
+
+const Custom = ({ children }) => {
+  const { isLoading } = useLoadUserQuery();
+  return (
+    <>
+      {
+        isLoading ? <h1>loading.....</h1> :
+          <>
+            {children}
+          </>
+
+
+      }
+    </>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <Provider store={appStore}>
-  <App />
-  <Toaster position='top-center'/>
+    <Provider store={appStore}>
+      <Custom>
+        <App />
+        <Toaster position='top-center' />
+
+  </Custom>
   </Provider>
-  </StrictMode>,
+  </StrictMode >,
 )
